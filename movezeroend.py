e04350dd65ae157a90db61bb721c063151ad7810
Yogitha <yogithasandula21@gmail.com>
2026-07-14 22:04:09 +0530

    Update movezeroend.py

--- movezeroend.py
+++ movezeroend.py
@@ -7,4 +7,12 @@ for i in range(len(arr)):
 for i in range(pos,len(arr)):
     arr[i]=0
 
-print(arr)            
+print(arr)  
+
+# another solution 
+arr=[1,0,2,0,3,0]
+j=0
+for i in range(len(arr)):
+    if arr[i]!=0:
+    arr[i],arr[j]=arr[j],arr[i]
+     j+=1
